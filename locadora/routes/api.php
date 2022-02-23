8569f2589bb3ecf61ff14e359ee08e9d1caa9d8f
fabiozanchettin <fabiozan@gmail.com>
2022-02-23 06:26:22 -0400

    rotas

--- locadora/routes/api.php
+++ locadora/routes/api.php
@@ -19,8 +19,27 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 //Route::resource('cliente', 'App\Http\Controllers\ClienteController');
-Route::apiResource('cliente', 'App\Http\Controllers\ClienteController');
-Route::apiResource('carro', 'App\Http\Controllers\CarroController');
-Route::apiResource('locacao', 'App\Http\Controllers\LocacaoController');
-Route::apiResource('marca', 'App\Http\Controllers\MarcaController');
-Route::apiResource('modelo', 'App\Http\Controllers\ModeloController');
+Route::apiResource('cliente', 'App\Http\Controllers\ClienteController')->middleware('jwt.auth');
+Route::apiResource('carro', 'App\Http\Controllers\CarroController')->middleware('jwt.auth');
+Route::apiResource('locacao', 'App\Http\Controllers\LocacaoController')->middleware('jwt.auth');
+Route::apiResource('marca', 'App\Http\Controllers\MarcaController')->middleware('jwt.auth');
+Route::apiResource('modelo', 'App\Http\Controllers\ModeloController')->middleware('jwt.auth');
+
+    Route::post('login', 'App\Http\Controllers\AuthController@login');
+    Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+    Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+    Route::post('me', 'App\Http\Controllers\AuthController@me');
+/*Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'App\Http\Controllers\AuthController@login');
+    Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+    Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+    Route::post('me', 'App\Http\Controllers\AuthController@me');
+
+});
+*/
