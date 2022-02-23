0e465381afa0ebad4189d18d80741d4fff77d6de
fabiozanchettin <fabiozan@gmail.com>
2022-02-23 09:06:31 -0400

    vue nao funciona

--- locadora/resources/views/auth/login.blade.php
+++ locadora/resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    <login-login>merdaaaaaa</login-login>
+    <login-login></login-login>
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
